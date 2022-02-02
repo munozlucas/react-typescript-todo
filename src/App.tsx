a289c65bae512f3b193ea188aa973e19b0d0101f
munozlucas <lgaston.munoz@gmail.com>
2022-02-02 19:06:04 -0300

    todo form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
 
-const App: React.FC= () => {
+const App: React.FC = () => {
   const todos = [{ id: "t1", text: "Finish the course" }];
   return (
     <div>
+      <NewTodo />
       <TodoList items={todos} />
     </div>
   );
